9f5e35d89e746ae2b36f72bdddf36e1a82b128e6
benjo16 <benyitocch@gmail.com>
2018-01-12 18:47:57 -0400

    sistema_de_reservas

--- node_modules/dsg-ng2-bs4-modal/components/modal-header.d.ts
+++
@@ -1,9 +0,0 @@
-import { ModalComponent } from './modal';
-export declare class ModalHeaderComponent {
-    private modal;
-    showClose: boolean;
-    headerClasses: {
-        [key: string]: string;
-    };
-    constructor(modal: ModalComponent);
-}
